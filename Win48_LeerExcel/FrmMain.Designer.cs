37ae577070f642417653f3562410b4ca4be2374b
Fabsys <farroyo@fabsys.net>
2021-07-01 14:38:48 -0600

    Ingreso del Proyecto

---
+++ Win48_LeerExcel/FrmMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Win48_LeerExcel
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblFindFile = new System.Windows.Forms.Label();
+            this.dtgData = new System.Windows.Forms.DataGridView();
+            this.gpbMain = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).BeginInit();
+            this.gpbMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDir
+            // 
+            this.txtDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDir.Location = new System.Drawing.Point(43, 16);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(593, 20);
+            this.txtDir.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRead.Location = new System.Drawing.Point(641, 15);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblFindFile
+            // 
+            this.lblFindFile.AutoSize = true;
+            this.lblFindFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFindFile.Location = new System.Drawing.Point(6, 20);
+            this.lblFindFile.Name = "lblFindFile";
+            this.lblFindFile.Size = new System.Drawing.Size(31, 13);
+            this.lblFindFile.TabIndex = 2;
+            this.lblFindFile.Text = "File:";
+            this.lblFindFile.Click += new System.EventHandler(this.lblFindFile_Click);
+            // 
+            // dtgData
+            // 
+            this.dtgData.AllowUserToAddRows = false;
+            this.dtgData.AllowUserToDeleteRows = false;
+            this.dtgData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgData.Location = new System.Drawing.Point(17, 66);
+            this.dtgData.Name = "dtgData";
+            this.dtgData.ReadOnly = true;
+            this.dtgData.Size = new System.Drawing.Size(722, 359);
+            this.dtgData.TabIndex = 3;
+            // 
+            // gpbMain
+            // 
+            this.gpbMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gpbMain.Controls.Add(this.lblFindFile);
+            this.gpbMain.Controls.Add(this.txtDir);
+            this.gpbMain.Controls.Add(this.btnRead);
+            this.gpbMain.Location = new System.Drawing.Point(17, 12);
+            this.gpbMain.Name = "gpbMain";
+            this.gpbMain.Size = new System.Drawing.Size(722, 48);
+            this.gpbMain.TabIndex = 4;
+            this.gpbMain.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 437);
+            this.Controls.Add(this.gpbMain);
+            this.Controls.Add(this.dtgData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).EndInit();
+            this.gpbMain.ResumeLayout(false);
+            this.gpbMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblFindFile;
+        private System.Windows.Forms.DataGridView dtgData;
+        private System.Windows.Forms.GroupBox gpbMain;
+    }
+}
+
---
+++ Win48_LeerExcel/FrmMain.cs
@@ -0,0 +1,183 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win48_LeerExcel
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean ValidateRead()
+        {
+            String extension;
+
+            if (txtDir.Text.Trim() == "")
+            {
+                MessageBox.Show("Seleccione el archivo antes de continuar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return false;
+            }
+
+            if (!System.IO.File.Exists(txtDir.Text.Trim()))
+            {
+                MessageBox.Show("Archivo NO Existe", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return false;
+            }
+
+            extension = System.IO.Path.GetExtension(txtDir.Text.Trim());
+
+            if (!extension.ToUpper().Contains("XLS"))
+            {
+                MessageBox.Show("Archivo NO es válido", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Read()
+        {
+            String fileName;
+            IXLRows nonEmptyDataRows;
+            DataTable dataExcel;
+            Int32 currentRow;
+            Int32 lineCell;
+            String headerCell;
+            List<String> headerList;
+            Char charASCII;
+            Type vloTipoDato;
+
+            if (!ValidateRead())
+            {
+                return;
+            }
+
+            headerList = new List<String>();
+            fileName = txtDir.Text.Trim();
+            dataExcel = new DataTable("Datos");
+            currentRow = 1;
+            lineCell = 1;
+
+            using (var excelWorkbook = new XLWorkbook(fileName))
+            {
+                nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
+
+                foreach (IXLRow dataRow in nonEmptyDataRows)
+                {
+                    if (currentRow == 1)
+                    {
+                        do
+                        {
+                            try
+                            {
+                                headerCell = (String)dataRow.Cell(lineCell).Value;
+
+                                headerCell = headerCell.ToLower();
+
+                                for (Int32 currentRowASCII = 0; currentRowASCII < 255; currentRowASCII++)
+                                {
+                                    if (currentRowASCII < 97 || currentRowASCII > 122)
+                                    {
+                                        charASCII = (char)currentRowASCII;
+
+                                        headerCell = headerCell.Replace(charASCII.ToString(), "");
+                                    }
+                                }
+
+                                headerCell = headerCell.ToUpper();
+
+                                if (headerCell != "")
+                                {
+                                    headerList.Add(headerCell);
+                                }
+                            }
+                            catch { headerCell = String.Empty; }
+
+                            lineCell += 1;
+                        }
+                        while (headerCell != "");
+                    }
+
+
+                    try
+                    {
+                        if (currentRow >= 2)
+                        {
+                            if (dataExcel.Columns.Count == 0)
+                            {
+                                lineCell = 1;
+
+                                foreach (String vloTitulo in headerList)
+                                {
+                                    vloTipoDato = dataRow.Cell(lineCell).Value.GetType();
+
+                                    dataExcel.Columns.Add(vloTitulo, vloTipoDato);
+
+                                    lineCell += 1;
+                                }
+                            }
+
+                            lineCell = 1;
+
+                            dataExcel.Rows.Add();
+
+                            foreach (String headerCellColumna in headerList)
+                            {
+                                dataExcel.Rows[dataExcel.Rows.Count - 1][headerCellColumna] = dataRow.Cell(lineCell).Value;
+
+                                lineCell += 1;
+                            }
+                        }
+                    }
+                    catch { headerCell = String.Empty; }
+
+                    currentRow += 1;
+                }
+            }
+
+            dtgData.DataSource = dataExcel.Copy();
+
+            for (int i = 0; i <= dtgData.Columns.Count - 1; i++)
+            {
+                dtgData.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            Read();
+        }
+
+        private void lblFindFile_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.xls)|*.xls|All files (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    txtDir.Text = openFileDialog.FileName;
+                }
+            }
+        }
+    }
+}
